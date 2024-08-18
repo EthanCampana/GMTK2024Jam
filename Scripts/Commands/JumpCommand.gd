bf8221f776ce4208f498263bacb1140230d92a8c
Allan Raposo <araposo1@jh.edu>
2024-08-17 22:41:54 -0700

    temporary player movement

--- Scripts/Commands/JumpCommand.gd
+++
@@ -1,9 +0,0 @@
-class_name JumpCommand
-
-
-# TODO Coyote Jump Implementation and Jump Buffering
-func execute(player: Player, state: PlayerState) -> void:
-    match state:
-        _ when state is Idle or state is Move:
-            player.emit_signal("change_state","AIR",{})
-            
