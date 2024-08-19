b62513dee758c4d773dfbc4e131cb98707f3eace
Allan Raposo <araposo1@jh.edu>
2024-08-19 15:17:21 -0700

    initial movement and signals

--- Scripts/Commands/player.gd
+++ Scripts/Commands/player.gd
@@ -1,29 +1,28 @@
 extends CharacterBody2D
 class_name Player
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+enum Stats {SPEED = 0, JUMP = 1, WEIGHT = 2}
 # These are values that can change based on the stats
-var isAlive : bool = true	
-var spawnLocation : Vector2 = self.global_position
-@export var SPEED: int = 180
-@export var JUMP_FORCE: int = 255
-@export var GRAVITY: int = 900
-@export var WEIGHT: int = 60
+@export var speed_multiplier: int = 0
+@export var jump_force_multiplier: int = 0
+@export var weight_multiplier: int = 0
+@export var current_stat: Stats = Stats.SPEED
+@export var TOTAL_POINTS: int = 4
+
+var BASE_SPEED: int = 180
+var BASE_JUMP_FORCE: int = 255
+var BASE_GRAVITY: int = 900
+var BASE_WEIGHT: int = 60
+var STAT_COUNT: int = len(Stats)
 
 func _physics_process(delta):
-	if not isAlive:
-		$AnimatedSprite2D.play("die")
-		await animated_sprite_2d.animation_finished
-		animated_sprite_2d.visible = false
-		self.global_position = spawnLocation
-		animated_sprite_2d.visible = true
-
-		
-	var direction = Input.get_axis("move_left", "move_right")
+	""" Handles all the input functionalities and physics """
 	# Horizontal movement
+	var direction = Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED - (0.20 * WEIGHT)
-		$AnimatedSprite2D.play("run")
+		velocity.x = direction * BASE_SPEED
+		if is_on_floor():
+			$AnimatedSprite2D.play("run")
 	else:
 		velocity.x = 0
 		if is_on_floor():
@@ -37,28 +36,66 @@ func _physics_process(delta):
 	
 	# Gravity
 	if not is_on_floor():
-		velocity.y += GRAVITY * delta
+		velocity.y += BASE_GRAVITY * delta
 		if velocity.y > 0:
 			$AnimatedSprite2D.play("fall")
 	
 	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y -= JUMP_FORCE - (0.1 * WEIGHT)
+		velocity.y -= BASE_JUMP_FORCE - (0.1 * BASE_WEIGHT)
 		$AnimatedSprite2D.play("jump")
-	
-	move_and_slide()
-	
 		
+	# Scroll through the stats
+	if Input.is_action_just_pressed("stat_up"):
+		set_stat_to_change(-1)
+	elif Input.is_action_just_pressed("stat_down"):
+		set_stat_to_change(1)
+		
+	# increase or decrease stat
+	if Input.is_action_just_pressed("decrease"):
+		change_stat_value(-1)
+	elif Input.is_action_just_pressed("increase"):
+		change_stat_value(1)
+	move_and_slide()
 
+func set_stat_to_change(vertical_direction: int) -> void:
+	""" Changes the currently selected stat """
+	var previous: int = current_stat
+	var normalized_index = (current_stat + vertical_direction) % STAT_COUNT
 
-func _on_area_2d_body_entered(body:Node2D) -> void:
-	if body is DynamicTileMapLayer:
-		body.has_entered_dyanmic_tile = true
-		body.player = self
+	if normalized_index < 0:
+		normalized_index += STAT_COUNT
+	
+	current_stat = normalized_index
+	UiController.emit_signal("stat_selection_changed",current_stat)
 
+func get_total_points_allocated() -> int:
+	""" Gets total points currently allocated """
+	return speed_multiplier + jump_force_multiplier + weight_multiplier
 
-
-func _on_area_2d_body_exited(body:Node2D) -> void:
-	if body is DynamicTileMapLayer:
-		body.has_entered_dyanmic_tile = false 
-		body.player = null 
+func can_increase_stat(stat_value: int) -> bool:
+	""" Determines whether the stat could be increased """
+	return get_total_points_allocated() < TOTAL_POINTS and stat_value < TOTAL_POINTS
+	
+func can_decrease_stat(stat_value: int) -> bool:
+	""" Determines whether the stat could be decreased """
+	return stat_value > 0
+	
+func change_stat_value(change: int) -> void:
+	""" Changes the value of the stat if it meets the right conditions. 
+	
+	Must follow these principles:
+		1) a stat cannot be negative
+		2) a stat cannot exceed total points
+		3) the sum of all stats cannot exceed total points
+	"""
+	var change_func: Callable = can_increase_stat if change == 1 else can_decrease_stat
+	if current_stat == Stats.SPEED and change_func.call(speed_multiplier):
+		speed_multiplier += change
+		UiController.emit_signal("stat_changed", Stats.SPEED, speed_multiplier)
+	elif current_stat == Stats.JUMP and change_func.call(jump_force_multiplier):
+		jump_force_multiplier += change
+		UiController.emit_signal("stat_changed", Stats.JUMP, jump_force_multiplier)
+	elif current_stat == Stats.WEIGHT and change_func.call(weight_multiplier):
+		weight_multiplier += change
+		UiController.emit_signal("stat_changed", Stats.WEIGHT, weight_multiplier)
